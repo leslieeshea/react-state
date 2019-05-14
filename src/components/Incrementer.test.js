9a744e85c4e27524b1ae875ed8b8beaeb01b5ca2
Leslie Shea <leslieyshea@gmail.com>
2019-05-14 13:22:32 -0700

    incremements count on button click test passing

--- src/components/Incrementer.test.js
+++ src/components/Incrementer.test.js
@@ -7,4 +7,10 @@ describe('Incrementer component', () => {
     const wrapper = shallow(<Incrementer />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('increments count on button click', () => {
+    const wrapper = shallow(<Incrementer />);
+    wrapper.find('button').at(0).simulate('click');
+    expect(wrapper.state('count')).toEqual(1);
+  });
 });
