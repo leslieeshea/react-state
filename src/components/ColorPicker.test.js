a5951a0d3b8765765a80cd5c9cb7349368d2e420
Leslie Shea <leslieyshea@gmail.com>
2019-05-14 11:45:45 -0700

    color picker snapshot test passing

---
+++ src/components/ColorPicker.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ColorPicker from './ColorPicker';
+
+describe('ColorPicker component', () => {
+  it('renders a ColorPicker', () => {
+    const wrapper = shallow(<ColorPicker />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
